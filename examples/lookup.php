aab579f0dd60f4fa438e450d1ad672b648a187f0
Mike Almond <almond@fastnorth.com>
2013-11-14 09:00:24 -0500

    Cleaned up the examples and added some comments

--- examples/lookup.php
+++ examples/lookup.php
@@ -1,14 +1,20 @@
 <pre><?php
 
-use MusicBrainz\MusicBrainz;
 use Guzzle\Http\Client;
+use MusicBrainz\MusicBrainz;
 
-require_once '../vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-//Create new phpBrainz object
+//Create new MusicBrainz object
 $brainz = new MusicBrainz(new Client(), 'username', 'password');
-$brainz->setUserAgent('ApplicationName', '0.1', 'http://example.com');
+$brainz->setUserAgent('ApplicationName', '0.2', 'http://example.com');
 
+
+/**
+ * Lookup an Artist and include a list of Releases, Recordings, Release Groups and User Ratings
+ * Note: You must be logged in to retrieve user-ratings
+ * @see http://musicbrainz.org/doc/Artist
+ */
 $includes = array(
     'releases',
     'recordings',
@@ -16,14 +22,18 @@ $includes = array(
     'user-ratings'
 );
 try {
-    $artist = $brainz->lookup('artist', '4dbf5678-7a31-406a-abbe-232f8ac2cd63', $includes);  //bryan adams
+    $artist = $brainz->lookup('artist', '4dbf5678-7a31-406a-abbe-232f8ac2cd63', $includes);
     print_r($artist);
 } catch (Exception $e) {
     print $e->getMessage();
 }
-
 print "\n\n";
 
+
+/**
+ * Lookup a Release Group based on an MBID
+ * @see http://musicbrainz.org/doc/Release_Group
+ */
 try {
     //born this way: the remix
     $releaseGroup = $brainz->lookup('release-group', 'e4307c5f-1959-4163-b4b1-ded4f9d786b0');
